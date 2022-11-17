ef26da3b60a257c724d76f48092653fcdce32b15
Hiago Maciel <hiago138@hotmail.com>
2022-11-17 15:51:49 -0300

    Build Package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,20 @@
-export declare const Greeter: (name: string) => string;
+import { UserCredential, NextOrObserver, User } from 'firebase/auth';
+export type FirebaseConfig = {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId: string;
+};
+declare const useFirebase: (config: FirebaseConfig) => {
+    createUser: (email: string, password: string) => Promise<UserCredential>;
+    login: (email: string, password: string) => Promise<UserCredential>;
+    logout: () => Promise<void>;
+    resetPassword: (email: string) => Promise<void>;
+    getToken: (forceRefresh?: boolean) => Promise<string> | undefined;
+    onAuthChanged: (observer: NextOrObserver<User>) => import("@firebase/util").Unsubscribe;
+    changePassword: (pass1: string, pass2: string, oldPass: string) => Promise<UserCredential>;
+};
+export default useFirebase;
