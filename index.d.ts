19eee1375ce0d5610ee7ea71b2bf3f7fc7042dd9
Hiago Maciel <hiago138@hotmail.com>
2022-11-18 15:49:35 -0300

    last

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,6 @@
 import { signInWithEmailAndPassword, signOut, UserCredential, Auth } from 'firebase/auth';
-declare const login: (auth: Auth, email: string, password: string) => Promise<UserCredential>;
-export { login, signInWithEmailAndPassword, signOut };
+declare const withAuth: (auth: Auth) => {
+    login: (email: string, password: string) => Promise<UserCredential>;
+    getToken: (forceRefresh?: boolean) => Promise<string> | undefined;
+};
+export { withAuth, signInWithEmailAndPassword, signOut };
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.signOut = exports.signInWithEmailAndPassword = exports.login = void 0;
+exports.signOut = exports.signInWithEmailAndPassword = exports.withAuth = void 0;
 var auth_1 = require("firebase/auth");
 Object.defineProperty(exports, "signInWithEmailAndPassword", { enumerable: true, get: function () { return auth_1.signInWithEmailAndPassword; } });
 Object.defineProperty(exports, "signOut", { enumerable: true, get: function () { return auth_1.signOut; } });
 // const createUser = async (auth: Auth, email: string, password: string) =>
 //   await createUserWithEmailAndPassword(auth, email, password);
-var login = function (auth, email, password) {
-    return (0, auth_1.signInWithEmailAndPassword)(auth, email, password);
+var withAuth = function (auth) {
+    var login = function (email, password) {
+        return (0, auth_1.signInWithEmailAndPassword)(auth, email, password);
+    };
+    var getToken = function (forceRefresh) { var _a; return (_a = auth === null || auth === void 0 ? void 0 : auth.currentUser) === null || _a === void 0 ? void 0 : _a.getIdToken(forceRefresh); };
+    return { login: login, getToken: getToken };
 };
-exports.login = login;
+exports.withAuth = withAuth;
