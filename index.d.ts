a6e2905ea5fdc09c7ddf9b38bbc2cb125c77741e
Hiago Maciel <hiago138@hotmail.com>
2022-11-18 16:15:31 -0300

    buildado

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,13 @@
-import { signInWithEmailAndPassword, signOut, UserCredential, Auth } from 'firebase/auth';
-declare const withAuth: (auth: Auth) => {
+import { UserCredential, NextOrObserver, User, Auth } from 'firebase/auth';
+declare const authFirebase: (auth: Auth) => {
+    createUser: (email: string, password: string) => Promise<UserCredential>;
     login: (email: string, password: string) => Promise<UserCredential>;
+    logout: () => Promise<void>;
+    resetPassword: (email: string) => Promise<void>;
+    getEmailCredential: (password: string) => import("@firebase/auth").EmailAuthCredential;
     getToken: (forceRefresh?: boolean) => Promise<string> | undefined;
+    onAuthChanged: (observer: NextOrObserver<User>) => import("@firebase/util").Unsubscribe;
+    changePassword: (newPass: string, confPass: string, oldPass: string) => Promise<UserCredential>;
+    currentUser: User | null;
 };
-export { withAuth, signInWithEmailAndPassword, signOut };
+export default authFirebase;
--- src/index.ts
+++ src/index.ts
@@ -13,44 +13,56 @@ import {
   Auth,
 } from 'firebase/auth';
 
-// const createUser = async (auth: Auth, email: string, password: string) =>
-//   await createUserWithEmailAndPassword(auth, email, password);
+const authFirebase = (auth: Auth) => {
+  const createUser = (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password);
 
-const withAuth = (auth: Auth) => {
   const login: (email: string, password: string) => Promise<UserCredential> = (email, password) =>
     signInWithEmailAndPassword(auth, email, password);
+
+  const logout = () => signOut(auth);
+
+  const resetPassword = (email: string) => sendPasswordResetEmail(auth, email);
+
   const getToken = (forceRefresh?: boolean) => auth?.currentUser?.getIdToken(forceRefresh);
 
-  return { login, getToken };
+  const onAuthChanged = (observer: NextOrObserver<User>) => onAuthStateChanged(auth, observer);
+
+  const getEmailCredential = (password: string) => {
+    if (auth.currentUser?.email) {
+      return EmailAuthProvider.credential(auth.currentUser.email, password);
+    } else {
+      throw { status: 400, message: 'User not found' };
+    }
+  };
+
+  const changePassword = async (newPass: string, confPass: string, oldPass: string) => {
+    if (newPass !== confPass) {
+      throw { status: 400, message: 'Not match' };
+    }
+    if (newPass === oldPass) {
+      throw { status: 400, message: 'Same password' };
+    }
+    const credential = getEmailCredential(oldPass);
+    if (auth.currentUser) {
+      const authCred = await reauthenticateWithCredential(auth.currentUser, credential);
+      await updatePassword(auth.currentUser, newPass);
+      return authCred;
+    } else {
+      throw { status: 403, message: 'User not found' };
+    }
+  };
+
+  return {
+    createUser,
+    login,
+    logout,
+    resetPassword,
+    getEmailCredential,
+    getToken,
+    onAuthChanged,
+    changePassword,
+    currentUser: auth.currentUser,
+  };
 };
 
-// const resetPassword = (email: string) => sendPasswordResetEmail(auth, email);
-
-// const onAuthChanged = (observer: NextOrObserver<User>) => onAuthStateChanged(auth, observer);
-
-// const reauthUser = (password: string) => {
-//   if (auth.currentUser?.email) {
-//     return EmailAuthProvider.credential(auth.currentUser.email, password);
-//   } else {
-//     throw { status: 400, message: 'User not found' };
-//   }
-// };
-
-// const changePassword = async (pass1: string, pass2: string, oldPass: string) => {
-//   if (pass1 !== pass2) {
-//     throw { status: 400, message: 'Not match' };
-//   }
-//   if (pass1 === oldPass) {
-//     throw { status: 400, message: 'Same password' };
-//   }
-//   const credential = reauthUser(oldPass);
-//   if (auth.currentUser) {
-//     const authCred = await reauthenticateWithCredential(auth.currentUser, credential);
-//     await updatePassword(auth.currentUser, pass1);
-//     return authCred;
-//   } else {
-//     throw { status: 403, message: 'User not found' };
-//   }
-// };
-
-export { withAuth, signInWithEmailAndPassword, signOut }; // resetPassword, getToken, onAuthChanged, changePassword
+export default authFirebase;
